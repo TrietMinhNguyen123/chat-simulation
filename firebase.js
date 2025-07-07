2f1c2f433d1e9f5f4014eecbe6cfbb209b9c9611
Ctech 21M <tnguyen301@bostonk12.org>
2025-07-07 15:25:56 -0400

    initial commit

---
+++ firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCqzEPnLwOxozpdHsoVOo0Jmbvao9M4nvY",
+    authDomain: "bot-simulation-95f71.firebaseapp.com",
+    projectId: "bot-simulation-95f71",
+    storageBucket: "bot-simulation-95f71.firebasestorage.app",
+    messagingSenderId: "865109523752",
+    appId: "1:865109523752:web:a509e4eca06bdff0a5641d",
+    measurementId: "G-614JL4H2K5"
+};
+
+const app = initializeApp(firebaseConfig);
