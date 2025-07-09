a8a50690f5ce56af69da0a7a61c7ed3e7285c176
TrietMinhNguyen123 <tnguyen301@bostonk12.org>
2025-07-09 17:10:56 -0400

    day 3

--- src/pages/ChatBot.js
+++ src/pages/ChatBot.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import '../pages/styles/Chatbot.css';
 import '../../src/index.css'
-import search_glass from "../img/search_glass.png";
+import sendButton from "../img/send.png"
 function ChatBot() {
   return (
     <div className="ChatPage">
         <div className='logo'>Chat simulation</div>
         <div className='Side_bar'></div>
         <div className="chat_interaction">
-            <div className='Search_location'>
-            <input className='Search_bar' type="text" placeholder="Search..">
-            </input>
-              <img src={search_glass}></img>
-            </div>
+        <input className='Search_bar2' type="text" placeholder="Search your answer.."/>
+          <img class="sendButton2" src={sendButton}></img>
         </div>
     </div>
   );
