a05ca7d4923d8623467391f6d2aba76a35c3a538
Ctech 21M <tnguyen301@bostonk12.org>
2025-07-09 14:52:27 -0400

    adding tailwind

--- src/pages/ChatBot.js
+++ src/pages/ChatBot.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../pages/styles/Chatbot.css';
-
+import '../../src/index.css'
+import search_glass from "../img/search_glass.png";
 function ChatBot() {
   return (
     <div className="ChatPage">
@@ -8,7 +9,9 @@ function ChatBot() {
         <div className='Side_bar'></div>
         <div className="chat_interaction">
             <div className='Search_location'>
-            <input className='Search_bar' type="text" placeholder="Search.."></input>
+            <input className='Search_bar' type="text" placeholder="Search..">
+            </input>
+              <img src={search_glass}></img>
             </div>
         </div>
     </div>
